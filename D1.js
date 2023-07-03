811bb4e0529d3fb920b6ef50c3e38963bdbf786f
munteanu rita <munteanu.rita99@gmail.com>
2023-07-03 17:56:19 +0200

    correzione con la classe

--- D1.js
+++ D1.js
@@ -9,10 +9,12 @@ REGOLE
 
 /* ESERCIZIO 1
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
-*/ let titolo="testo, numero, vero o falso(boolean), null o indefined";
+*/ 
+const titolo="testo numero vero o falso null o undefined";
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-let testo="ciao bambini"
+
+/*let testo="ciao bambini"
 const numero= 5
 const veroOFalso
 let numero= null;
@@ -21,14 +23,15 @@ let x = true;
 let y = false;
 
 Queste sono le principali datatype in javascript, testo ti aiuta a scrivere qualssiasi tipo di testo, puoi scrivere nomi,città,luoghi ecc. Grazie al numero puoi 
-scrivere qualsiasi tipo di numero che sia intero o meno, vero o falso indichi se un valore deve essere vero o falso e con il null un valore non è niente.
+scrivere qualsiasi tipo di numero che sia intero o meno, vero o falso indichi se un valore deve essere vero o falso e con il null un valore non è niente. */
 
 /* ESERCIZIO 2
  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-let nome="Rita"; 
+const nome= "Rita"; 
+console.log(nome)
 
 
 /* ESERCIZIO 3
@@ -36,7 +39,9 @@ let nome="Rita";
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-console.log (nome+titolo)=Rita testo, numero, vero o falso(boolean), null o indefined
+const numero1= 12
+const numero2= 20
+console.log(numero1+numero2);
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
@@ -51,8 +56,7 @@ const x= 12
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-let nome;
-nome="Munteanu"
+const nome ="Rita"
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
